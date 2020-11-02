f64b5b6fefe61eb4374738fc969e8c26ae0a9b87
Rin Yokoyama <ryokoyam@utk.edu>
2020-11-02 12:52:27 -0500

    modID is replaced with LENSLongLiquidScintillatorGammaHit::GetModuleID() in the CloverSimEventAction

--- cloversim/source/CloverSimEventAction.cc
+++ cloversim/source/CloverSimEventAction.cc
@@ -44,15 +44,14 @@ void CloverSimEventAction::EndOfEventAction(const G4Event* evt)
                 auto hc = (LENSLongLiquidScintillatorGammaHitsCollection*)hitCollections->GetHC(i);
                 auto nhits = hc->entries();
                 clover_hit_ = CLOVER_HIT_DEFAULT_STRUCT;
-                clover_hit_.modId = hc->GetModuleId();
-                G4double energy = 0;
+                clover_hit_.modE = 0;
                 //std::cout << "CloverSingle n_hits: " << nhits << std::endl;
                 for (int i=0; i<nhits; ++i) {
                     auto hit = (LENSLongLiquidScintillatorGammaHit*)hc->GetHit(i);
-                    energy += hit->GetEnergyDeposited();
+                    clover_hit_.modE += hit->GetEnergyDeposited();
+                    clover_hit_.modId = hit->GetModuleID();
                     //std::cout << " modId: " << clover_hit_.modId << " modE: " << clover_hit_.modE;
                 }
-                clover_hit_.modE = energy;
                 tree_data_->clover_hits_.emplace_back(clover_hit_);
             }
         }
